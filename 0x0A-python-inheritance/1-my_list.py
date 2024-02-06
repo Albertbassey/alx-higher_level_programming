c3ba797ec5401540b051df8d609e4959fee63b10
Albertbassey <aniediongetim@gmail.com>
2024-02-05 20:10:16 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-contains a MyList class
-"""
+"""Write a class MyList that inherits from list
+    """
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """Write a class MyList that inherits from list
+    """
+    pass
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """that prints the list, but sorted (ascending sort)
+        """
         print(sorted(self))
